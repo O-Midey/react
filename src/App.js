d554e70839661c536fcf07adf72c94a8d50a64fb
O-Midey <46810599+O-Midey@users.noreply.github.com>
2022-08-13 20:44:43 +0100

    passed a parent prop to child

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Counter from "./components/Counter"
 import FunctionClick from "./components/FunctionClick"
 import ClassClick from "./components/ClassClick"
 import EventBind from "./components/EventBind"
+import ParentComponent from "./components/ParentComponent";
 
 
 
@@ -18,6 +19,7 @@ class App extends Component {
       <div className="App">
         {/* <FunctionClick/>
         <ClassClick/> */}
+        <ParentComponent />
       </div>
     );
   }
